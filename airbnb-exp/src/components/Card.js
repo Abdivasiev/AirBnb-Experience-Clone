11b57739634d417754fc563e6e54e73722737af3
Miran Abdivasiev <abdivasiev@gmail.com>
2022-08-23 10:00:20 +0600

    Final
    Finished project. Needs to: add meaningful Readme + delete deprecated files.

---
+++ airbnb-exp/src/components/Card.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Star from "../images/star.png";
+
+export default function Card(props) {
+  let badgeText;
+  if (props.item.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.item.location === "Online") {
+    badgeText = "ONLINE";
+  }
+
+  return (
+    <div className="card">
+      {badgeText && <div className="card--badge">{badgeText}</div>}
+      <img
+        src={"/images/" + props.item.coverImg}
+        className="card--photo"
+        alt="Cover"
+      />
+      <div className="card--stats">
+        <img src={Star} className="card--star" alt="Star" />
+        <span className="card--rating">{props.item.stats.rating} </span>
+        <span className="gray">({props.item.stats.reviewCount}) • </span>
+        <span className="gray">{props.item.location}</span>
+      </div>
+      <p className="card--title">{props.item.title}</p>
+      <p>
+        <span className="bold">From ${props.item.price}</span> / person
+      </p>
+    </div>
+  );
+}
---
+++ airbnb-exp/src/components/Hero.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ImageGrid from "../images/photo-grid.png";
+
+export default function Hero() {
+  return (
+    <section className="hero">
+      <img src={ImageGrid} alt="grid" className="hero--image" />
+      <h1 className="hero--title">Online Experiences</h1>
+      <p className="hero--text">
+        Join unique interactive activities led by one-of-a-kind hosts - all
+        without leaving home
+      </p>
+    </section>
+  );
+}
